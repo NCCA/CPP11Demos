7954aaf2f3f0630afb4de7697b5d99c63dc85351
jmacey <jmacey@bournemouth.ac.uk>
2016-10-17 12:49:44 +0100

    updated to C++11 array

--- constexprClass.cpp
+++ constexprClass.cpp
@@ -2,29 +2,27 @@
 #include <cstdlib>
 #include <cmath>
 
-
-
 class Circle
 {
-    public:
-    	constexpr Circle (float _x, float _y, float _radius) : 
-    m_x( _x ), m_y( _y ), m_radius( _radius ) {  }
-    constexpr double getArea () const
+  public:
+    constexpr Circle(float _x, float _y, float _radius) : m_x(_x), m_y(_y), m_radius(_radius) {}
+    constexpr double getArea() const
     {
         return m_radius * m_radius * M_PI;
     }
-    private:
-        float m_x=0.0f;
-        float m_y=0.f;
-        float m_radius=1.0f;
+
+  private:
+    float m_x = 0.0f;
+    float m_y = 0.f;
+    float m_radius = 1.0f;
 };
 
 int main()
 {
-	constexpr Circle c( 0.0, 0.0, 10.5 );
-	constexpr double area = c.getArea();
-	std::cout<<area<<"\n";
-	Circle c2(0.0,0.0,2.4);
-	std::cout<<c2.getArea()<<"\n";
-	return EXIT_SUCCESS;
+    constexpr Circle c(0.0, 0.0, 10.5);
+    constexpr double area = c.getArea();
+    std::cout << area << '\n';
+    Circle c2(0.0, 0.0, 2.4);
+    std::cout << c2.getArea() << '\n';
+    return EXIT_SUCCESS;
 }
--- enum3.cpp
+++ enum3.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <array>
 
 enum class DIRECTION {UP,DOWN,LEFT,RIGHT};
 
 int main()
 {
-	int data[4]={0,1,2,3};
+	std::array<int,4> data={{0,1,2,3}};
 	std::cout<<data[static_cast<int>(DIRECTION::UP)]<<"\n";
 }
